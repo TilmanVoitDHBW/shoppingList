1a5dc7982967b68ceaaf8c823a9e3815a9d5604a
TilmanVoit <tilman-voit@t-online.de>
2025-11-01 15:16:28 +0100

    list download hinzugefügt

--- src/components/shopping-list-accordion.tsx
+++ src/components/shopping-list-accordion.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
-import { Plus, Users, Trash2, Key } from "lucide-react"
+import { Plus, Users, Trash2, Key, Download } from "lucide-react"
 import { AddItemDialog } from "@/components/add-item-dialog"
 import { AddUserDialog } from "@/components/add-user-dialog"
 import { getItems } from "@/actions/getItems"
@@ -85,7 +85,10 @@ export function ShoppingListAccordion({list, onAddItem, onRemoveItem, onAddUser 
                   <Trash2 className="h-4 w-4" />
                   Delete List
                 </Button>
-
+                <Button onClick={() => downloadList(items)} size="sm" variant="outline" className="gap-2">
+                  <Download className="h-4 w-4" />
+                  Download List
+                </Button>
               </div>
 
               {items.length === 0 ? (
@@ -141,3 +144,25 @@ function setLoading(arg0: boolean) {
   throw new Error("Function not implemented.")
 }
 
+function downloadTextFile(filename: string, text: string) {
+  const blob = new Blob([text], { type: "text/plain" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
+
+
+function downloadList(items: ShoppingItem[]){
+  var text = ""
+  items.forEach(item => {
+    text += `- ${item.productName}:    ${item.amount} ${item.measurementUnit}\n`    
+  });
+  downloadTextFile("yourShoppinglist.txt", text)
+}
